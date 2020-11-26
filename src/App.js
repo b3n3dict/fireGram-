17a66af5b5a047c1bb1e5dd67234b57720a46285
Benedict Xavier <benedict.xavier.e@gmail.com>
2020-11-27 00:18:14 +0530

    imageGrid component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageGrid from './comps/ImageGrid';
 import Title from './comps/Title';
 import UploadForm from './comps/UploadForm'
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Title/>
       <UploadForm/>
+      <ImageGrid/>
     </div>
   );
 }
